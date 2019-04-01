a5fb88823df648d0bf08fb2af9bb3710b6573997
Becca Barton <becca.barton@voxmedia.com>
2019-04-01 10:52:28 -0400

    fix base

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,9 +1,9 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAer6eLeMteldz_ftCIMFaS1iVuXg6ouPA",
-    authDomain: "react-testing-b3639.firebaseapp.com",
-    databaseURL: "https://react-testing-b3639.firebaseio.com"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: ""
 });
 
 const base = Rebase.createClass(firebaseApp.database());
